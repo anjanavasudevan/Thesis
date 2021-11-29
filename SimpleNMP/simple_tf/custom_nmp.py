87e906d6e2fcdb1870e21d85cdda6d87daeaf5a1
anjanavasudevan <anjume24@gmail.com>
2021-11-29 08:17:40 +0530

    custom tf file added

---
+++ SimpleNMP/simple_tf/custom_nmp.py
@@ -0,0 +1,12 @@
+"""
+@author: Anjana Vasudevan
+"""
+# Simple NMP transfer function
+
+# Import dependencies
+from gym import Env
+from gym.spaces import Discrete, Box
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
