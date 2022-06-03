a02cfce1f874e75e1fb4d4eab35653bc0d87491d
anjanavasudevan <anjume24@gmail.com>
2022-06-03 20:01:54 +0530

    Solving environment

--- Quadrotor_simulation/environment.py
+++ Quadrotor_simulation/environment.py
@@ -6,15 +6,7 @@ Author: Anjana Vasudevan
 import gym
 import numpy as np
 from gym.spaces import Box
-
-
-def linear_model(x, A, B, u):
-    """
-    Evaluate linear model using the state space form
-    ẋ = Ax + Bu
-    """
-    dxdt = A@x + B@u
-    return dxdt
+from scipy.integrate import solve_ivp
 
 # Create gym class
 class hover(gym.Env):
@@ -56,8 +48,8 @@ class hover(gym.Env):
         self.D = np.zeros((3, 4))
 
         # Define action bounds (includes bias voltage to prevent motor burn)
-        self.u_min = np.float(np.array[2, 2, 2, 2])
-        self.u_max = np.float(np.array(24, 24, 24, 24))
+        self.u_min = np.array[2, 2, 2, 2]
+        self.u_max = np.array(24, 24, 24, 24)
 
         self.action_space = Box(low=self.u_min, high=self.u_max)
 
@@ -72,17 +64,52 @@ class hover(gym.Env):
         self.ang_rate_max = 60*np.pi/180
 
         # Specifying the observation space
-        self.x_max = np.float(np.array[self.p_max, self.r_max, self.y_max, \
-                        self.ang_rate_max, self.ang_rate_max, self.ang_rate_max])
-        self.x_min = np.float(np.array([-self.p_max, -self.r_max, self.y_min, 0, 0, 0]))
+        self.x_max = np.array[self.p_max, self.r_max, self.y_max, \
+                        self.ang_rate_max, self.ang_rate_max, self.ang_rate_max]
+        self.x_min = np.array([-self.p_max, -self.r_max, self.y_min, 0, 0, 0])
 
         self.observation_space = Box(low=self.x_min, high=self.x_max)
+
+        # Set the time for simulation
+        self.tstart = 0
+        self.tmax = 25
+        self.current_time = 0
+        self.current_timestep = 0
+        self.step_interval = 0.02
+
+        # Initial State-action
+        self.state = np.array([0, 0, 0, 0, 0, 0])
+        self.action = np.array([2, 2, 2, 2])
 
     def step(self, action):
         """
         Apply control to the motor
         """
-        # Make sure the action is 6*6 vector
+        # Make sure the action is size 4 vector
+        assert action.shape == self.action_space.shape
+
+        # Determine the current time
+        self.current_time = self.current_timestep*self.step_interval
+        time_range = (self.current_time, self.current_time+self.step_interval)
+
+        # Split the action vector:
+        u1, u2, u3, u4 = self.action
+
+        # Calculate the next step
+        solution = solve_ivp(self.linear_model, time_range, self.state, args=(u1, u2, u3, u4))
+
+        # 
+
+        # Calculate the next time
+    def linear_model(t, x, u1=2, u2=2, u3=2, u4=2):
+        """
+        Evaluate using state space representation
+        """
+        u = np.array([u1, u2, u3, u4])
+        dxdt = self.A@x + self.B@u
+        return dxdt
+
+
 
 
 
