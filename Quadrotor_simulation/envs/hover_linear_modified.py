8504b3e34f741653319dba067fa0709a32ce9a2e
anjanavasudevan <anjume24@gmail.com>
2022-06-09 18:31:58 +0530

    Modify hover env rewards

---
+++ Quadrotor_simulation/envs/hover_linear_modified.py
@@ -0,0 +1,22 @@
+"""
+Gym Environment for Quanser 3DOF Hover linear model with modifications in reward functions
+Author: Anjana Vasudevan
+"""
+import os
+import sys
+import gym
+import numpy as np
+from gym.spaces import Box
+
+class hover_state_space(gym.Env):
+    """
+    Hover model using the state space equations
+    """
+    def __init__(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
